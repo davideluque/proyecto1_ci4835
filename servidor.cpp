b11ffc2bf799ae11a8d0a8aa3dc2da9e177def5a
David Cabeza <cabezadavide@gmail.com>
2018-02-19 01:59:19 -0400

    error method. server method. two new structs

--- servidor.cpp
+++ servidor.cpp
@@ -22,18 +22,48 @@
 
 pthread_mutex_t mutex; // prevent race condition on critical sections.
 
-int main(int argc, char *argv[]){
+/**
+   method that shows error message and exit the program when an error occurs.
 
+   @param message -> error message
+*/
+void error(const char *message){
+	perror(message);
+	exit(EXIT_FAILURE);
+}
+
+void server(int port_number){
 	int socketfd; // socket file descriptor
 	int new_socket; // new connection socket file descriptor.
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
 
-	// create socket with params domain: IPv4 (AF_INET), type: TCP (SOCK_STREAM), 
-	// protocol: IP (0)
+	/**
+	   create socket with params:
+	     domain -> IPv4 (AF_INET)
+	     type -> TCP (SOCK_STREAM)
+	     protocol -> IP (0) 
+	*/ 
 	socketfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socketfd < 0){
-		perror("Error al crear el socket");
-		exit(EXIT_FAILURE);
-	}
+	if (socketfd < 0)
+		error("Error al crear el socket");
+
+	/* server_addr structure must be set to use in bind method call */
+
+	// byte order to use -> IPV4
+	server_addr.sin_family = AF_INET;
+
+	// bind the server to the localhost
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	// port must be converted from integer value into network byte order
+	server_addr.sin_port = htons(port_number);
+
+}
+
+int main(int argc, char *argv[]){
+
+	server(65500);
 
 	return 0;
 }
