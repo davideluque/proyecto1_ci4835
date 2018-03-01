4548b0064e78e7d0779bd76cbafbbb25f53f87eb
martinezfabiola <fabimartinezperez@gmail.com>
2018-03-01 09:07:42 -0400

    comandos servidor

--- servidor.cpp
+++ servidor.cpp
@@ -23,6 +23,7 @@
 #include <iostream> // cout
 #include <arpa/inet.h>  // sockaddr_in, inet_ntoa
 #include <string.h> // memset
+#include <vector>
 
 
 using namespace std;
@@ -46,6 +47,20 @@ ConnectionThread::ConnectionThread(int port_number, int socket){
 	port = port_number;
 	new_socketfd = socket;
 }
+
+class Books{
+public:
+	string book_name;
+	int downloads_num;
+	vector<string> client_book_list; 	
+};
+
+class Statistic{
+public:
+	vector<Books> downloaded_books;
+	vector<string> client_list;
+	
+};
 
 /**
    method that shows error message and exit the program when an error occurs.
@@ -91,17 +106,58 @@ void* handle_client(void *conn_thread){
 				send_list(new_socketfd);
 				break;
 			case 2:
+				read(new_socketfd, &command_solicitud, sizeof(command_solicitud));
+				printf("%d\n", command_solicitud);
 				break;
 			case 3:
+
 				break;
 			case 4:
 				close(new_socketfd);
 				// Kill thread.
 				break;
-		}
-		
+		}	
 	}
+}
 
+void server_help(){
+  std::cout << "> LIBROS_DESCARGADOS: muestra una lista de libros que han sido descargados por el cliente.\n";
+  std::cout << "> CLIENTE_QUE_CONSULTARON: muestra lista de clientes que han descargado libro.\n";
+  std::cout << "> NUM_DESCARGASxLIBROxCLIENTE: muestra el numero de descargas de un libro por cliente.\n";
+  std::cout << "> SALIR: termina la sesión\n";
+  std::cout << "----------------------------------------------------------------------------------\n";
+}
+
+void *show_statistics(void *unused){
+	std::string command;
+	std:: string statistics_command;
+	int command_num, data_size;
+
+	std::cout << "Bienvenido" << std::endl;
+	std::cout << "Escriba AYUDA si desea conocer la lista de comandos disponibles" << std::endl;
+
+	while(true){
+	std::cout << "Escriba el comando que desea ejecutar: ";
+    std::cin >> command;
+
+	    if(command == "AYUDA") server_help();
+	    else if (command == "LIBROS_DESCARGADOS"){
+	    	printf("%s \n", "LIBROS_DESCARGADOS");
+	    }
+	   
+	    else if (command == "CLIENTE_QUE_CONSULTARON"){
+	  		printf("%s \n", "CLIENTE_QUE_CONSULTARON");
+		}
+
+		else if (command == "NUM_DESCARGASxLIBROxCLIENTE"){
+			printf("%s \n", "NUM_DESCARGASxLIBROxCLIENTE");
+		}
+
+		else if (command == "SALIR"){
+	      exit(1);
+	    }
+	    else std::cout << "Comando inválido. Ejecute AYUDA si desea conocer la lista de comandos.\n";
+	}
 }
 
 /**
@@ -112,7 +168,8 @@ void* handle_client(void *conn_thread){
 void server(int port_number){
 	int socketfd; // socket file descriptor
 	int new_socketfd; // new connection socket file descriptor
-	int rc; // pthread creation return value
+	int rc_1; // pthread creation return value
+	int rc_2; // pthread creation return value
 	struct sockaddr_in server_addr;
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_len = sizeof(struct sockaddr_in);
@@ -156,6 +213,8 @@ void server(int port_number){
 	    Server should create a thread to handle either incomming connections or
 	    console inputs.
 	*/
+	pthread_t command_thread;
+	rc_2 = pthread_create(&command_thread, NULL, show_statistics,NULL);
 
 	// server is ready to wait for connections.
 	while(true){
@@ -166,7 +225,7 @@ void server(int port_number){
 
 		// connection successfully accepted 
 		std::cout 
-		<< "[CONEXIÓN] IP: " << inet_ntoa(client_addr.sin_addr) 
+		<< "\n[CONEXIÓN] IP: " << inet_ntoa(client_addr.sin_addr) 
 		<< " - PUERTO: " << ntohs(client_addr.sin_port) << std::endl;
 
 		// a new thread has to be created to handle the new connection
@@ -174,9 +233,9 @@ void server(int port_number){
 
 		ConnectionThread *ct = new ConnectionThread(port_number, new_socketfd);
 
-		rc = pthread_create(&conn_thread, NULL, handle_client, ct);
+		rc_1 = pthread_create(&conn_thread, NULL, handle_client, ct);
 
-		if (rc) error("Error al crear hilo para manejar la conexión");
+		if (rc_1) error("Error al crear hilo para manejar la conexión");
 
 	}
 
